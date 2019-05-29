82af49474e262f0f0f11c2c10de88f59c15228fe
Fatih Inan <fatihinan@gmail.com>
2019-05-29 10:44:36 +0200

    Add Aggressive Moto

---
+++ CoreEngine/Cards/CardsImpl/AggressiveMotoCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CoreEngine.Cards.CartTypes;
+
+namespace CoreEngine.Cards.CardsImpl
+{
+    public class AggressiveMotoCard : CharacterCard
+    {
+        public AggressiveMotoCard()
+        {
+            Name = "Aggressive Moto";
+            Clan = Clan.Unicorn;
+            Cost = 2;
+            Glory = 0;
+            Military = 3;
+            Political = 0;
+            Text = "This character cannot be declared as a defender.";
+            Traits = new[]
+            {
+                Trait.Bushi,
+                Trait.Cavalry
+            };
+            IsUnique = false;
+            ImageUrl = new Uri("http://lcg-cdn.fantasyflightgames.com/l5r/L5C01_111.jpg");
+            AllowedClans = new[]
+            {
+                Clan.Unicorn
+            };
+            DeckLimit = 3;
+            InfluenceCost = null;
+            IsRestricted = false;
+            Side = Side.Dynasty;
+        }
+    }
+}
--- CoreEngine/Cards/Trait.cs
+++ CoreEngine/Cards/Trait.cs
@@ -3,6 +3,7 @@
     public enum Trait
     {
         Bushi,
+        Cavalry,
         Condition,
         Fire,
         Shinobi,
