c6f9903380f82fef4fdd6243e8028dff70fd9012
Fatih Inan <fatihinan@gmail.com>
2019-05-29 10:55:47 +0200

    Add Akodo Gunso

---
+++ CoreEngine/Cards/CardsImpl/AkodoGunsoCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CoreEngine.Cards.CartTypes;
+
+namespace CoreEngine.Cards.CardsImpl
+{
+    public class AkodoGunsoCard : CharacterCard
+    {
+        public AkodoGunsoCard()
+        {
+            Name = "Akodo Gunsō";
+            Clan = Clan.Lion;
+            Cost = 2;
+            Glory = 2;
+            Military = 2;
+            Political = 1;
+            Text = "Pride. <i>(After this character wins a conflict, honor it. After this character loses a conflict, dishonor it.)</i>\\n<b>Reaction:</b> After this character enters play from a province – refill that province faceup.";
+            Traits = new[]
+            {
+                Trait.Bushi
+            };
+            IsUnique = false;
+            ImageUrl = new Uri("http://lcg-cdn.fantasyflightgames.com/l5r/L5C01_71.jpg");
+            AllowedClans = new[]
+            {
+                Clan.Lion
+            };
+            DeckLimit = 3;
+            InfluenceCost = null;
+            IsRestricted = false;
+            Side = Side.Dynasty;
+        }
+    }
+}
