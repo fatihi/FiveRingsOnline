702db27dec0face6ea7aeb5ff1cf98e3fcc888b7
Fatih Inan <fatihinan@gmail.com>
2019-08-12 22:47:54 +0200

    Add Bootstrap and Navigation

--- FiveRingsOnline/ClientApp/src/App.tsx
+++ FiveRingsOnline/ClientApp/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navigation from "./components/Navigation";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div>
+      <Navigation />
     </div>
   );
 };
